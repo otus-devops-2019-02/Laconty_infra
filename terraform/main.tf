32ced6e517443172e13bf3538eab5328c767a6fd
roman-mc <laconty.r@gmail.com>
2019-06-21 13:49:10 +0300

    Add terraform app, db files

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,9 @@ resource "google_compute_instance" "app" {
 
   network_interface {
     network       = "default"
-    access_config = {}
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
   }
 
   connection {
@@ -80,4 +82,8 @@ resource "google_compute_firewall" "firewall_ssh" {
   }
 
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
 }
