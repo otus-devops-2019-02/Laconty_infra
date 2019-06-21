f9575d385d99c2df05763f0d167c9b923a23cd0f
roman-mc <laconty.r@gmail.com>
2019-06-21 12:59:08 +0300

    Fix private key var; Import default-allow-ssh firewall rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,3 +69,15 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
