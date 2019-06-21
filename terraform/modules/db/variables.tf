40cc0d412ac6347acee7d2a55c3fb9122c6b8c4c
roman-mc <roman-mc@yandex-team.ru>
2019-06-21 18:18:18 +0300

    Remove commented config code

--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -11,23 +11,3 @@ variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
 }
-
-#
-# variable project {
-#   description = "Project ID"
-# }
-#
-# variable region {
-#   description = "Region"
-#
-#   default = "europe-west1"
-# }
-#
-# variable private_key_path {
-#   description = "Path to the private key used for ssh access"
-# }
-#
-# variable disk_image {
-#   description = "Disk image"
-# }
-
